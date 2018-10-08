9c4dc3bf0f371b70306a342e140c08c3bb89db1b
stephane.adjakotan <stephane.adjakotan@gmail.com>
2018-10-08 04:00:01 +0100

    background added

--- app/src/main/java/com/amstrong/stephane/artisanplus/activity/MainActivity.java
+++ app/src/main/java/com/amstrong/stephane/artisanplus/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.amstrong.stephane.artisanplus;
+package com.amstrong.stephane.artisanplus.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.amstrong.stephane.artisanplus.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
